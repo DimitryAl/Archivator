acc55bb2d159fa78dd262148e281ddb83323fb38
Dmitry <mr.dima.alexeev@gmail.com>
2021-02-24 22:59:28 +0300

    ...

--- main.c
+++ main.c
@@ -3,19 +3,22 @@
 #include <unistd.h>
 #include <sys/stat.h>
 
+#include "arc.c"
+#include "unarc.c"
+
 int main(int argc, char *argv[])
 {
     if (argc != 4)
     {
         printf("Wrong number of arguments!");
     }
-    if (strcam(argv[1], "-a") == 0) {
+    if (strcmp(argv[1], "-a") == 0) {
         arc(argv[2], argv[3], 0);
     }
-    if (strcam(argv[1], "-u") == 0) {
+    if (strcmp(argv[1], "-u") == 0) {
         unarc(argv[2], argv[3], 0);
     }
-    if (strcam(argv[1], "-u") != 0 || (strcam(argv[1], "-a") != 0)) {
+    if (strcmp(argv[1], "-u") != 0 || (strcmp(argv[1], "-a") != 0)) {
         printf("Wrong argument! Need -a or -u.");
     }
     return 0;
